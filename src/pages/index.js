b6808d0de7b6c9142760e45a721e2f5eb86f8a27
hollywilkalis <holly.wilkalis@gmail.com>
2021-10-31 19:40:15 -0700

    Tutorial step 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,8 @@
 // Step 1: Import React
 import * as React from 'react'
-import { Link } from 'gatsby'
+// import { Link } from 'gatsby'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 // Step 2: Define your component
 const IndexPage = () => {
@@ -9,6 +10,10 @@ const IndexPage = () => {
     <main>
       <Layout pageTitle="Home Page">
         <p>I'm making this by following the Gatsby Tutorial.</p>
+        <StaticImage
+        alt="Gus is a fluffy gray tabby. He is staring at the window at his enemy, the neighborhood squirrel"
+        src="../images/gus-and-the-squirrel.jpeg"
+      />
       </Layout>
     </main>
   )
